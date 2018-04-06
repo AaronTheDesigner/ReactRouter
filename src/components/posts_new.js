3e4690bbe20eb8bef846a982584e315a1bbc68db
AaronTheDesigner <flux.charlotte@gmail.com>
2018-04-05 21:06:34 -0400

    form structure, error handlers

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -2,30 +2,78 @@ import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
-    renderTitleField(field) {
+    renderField(field) {
+        const className = `form-group ${field.meta.touched && field.meta.error ? 'has-danger' : ''}`;
         return (
-            <div className="form-group">
-                <label>Title</label>
+            <div className={className}>
+                <label>{field.label}</label>
                 <input 
                     className="form-control"
                     type="text"
                     {...field.input}
                 />
+                <div className="text-help">
+                    {field.meta.touched ? field.meta.error: ''}
+                </div>
             </div>
         )
     }
+
+    onSubmit(values) {
+        console.log(values);
+    }
+
+
     render() {
+        const { handleSubmit } = this.props;
         return (
-        <form>
+        <form 
+            onSubmit={handleSubmit(this.onSubmit.bind(this))}
+            
+            >
             <Field 
+                label="Title"
                 name="title"
-                component={this.renderTitleField}
+                component={this.renderField}
             />
+            <Field 
+                label="Categories"
+                name="categories"
+                component={this.renderField}
+            />
+            <Field 
+                label="Content"
+                name="content"
+                component={this.renderField}
+            />
+            <button type="submit" className="btn btn-primary">Submit</button>
+            <button className="btn btn-alert">Cancel</button>
         </form>
         );
     }
 }
 
+function validate(values) {
+    //console.log(values) -> { title: '', categories: '', content: '' }
+    const errors = {};
+    // validate inputs from values object
+    if(!values.title) {
+        errors.title = "Enter a title!";
+    }
+    if(!values.categories) {
+        errors.categories = "Enter a category.";
+    }
+    if(!values.content) {
+        errors.content = "Enter content.";
+    }
+
+    //if error is empty the form is fine to submit
+    // if errors has any properties redux assumes form is invalid
+    return errors;
+
+}
+
 export default reduxForm({
+    validate,
     form: 'PostsNewForm'
 })(PostsNew);
